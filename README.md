c44bdf8af5e21da5fbdcf79cc8aed53d21d1858c
Kornel Sroka <srokakornel@gmail.com>
2018-04-23 22:34:12 +0200

    sit-on-chair

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Sit-On-Chair
+# H1 Sit On Chair
+Simple page made with:
+..* HTML
+..* CSS
+..* JS
+
+***
+
+Things to make:
+..* RWD
+..* more JS
---
+++ js/app.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log("DOM fully loaded");
+//
+// function offerSadow() {
+//         this.style.boxShadow = "2px 38px 61px -12px rgba(0,0,0,0.75)";
+//     }
+//
+//
+// var offerBox = document.querySelectorAll(".basic, .standard, .advance");
+//     // console.log(offerBox);
+//     for (var i = 0; i < offerBox.length; i++) {
+//         var singleOfferBox = offerBox[i];
+//         // console.log(singleOfferBox);
+//
+//         singleOfferBox.addEventListener("mousemove", offerSadow);
+//         singleOfferBox.removeEventListener("mouseout", offerSadowOff, false);
+//     };
+//
+// });
